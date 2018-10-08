6036d67192ba9dec3a8696c9b818e6291ad9a7f7
Deryk <derahner@outlook.com>
2018-10-07 19:20:47 -0700

    Problem 1 Incorrect, Problem 2 Started

--- Assignment 5 - Problem 2.py
+++ Assignment 5 - Problem 2.py
@@ -2,3 +2,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+"""Givens"""
+T = 293    #Temperature for everything else
+T_m = 298    #Temperature for mecury
+p = 1
+R_cr = 5*(10**5)
+
+"""Interpolation"""
+print("Key","[Air,Water,Isobutane,Mercury]")
+
+
+

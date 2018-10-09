1ccc5c90ffec7484fb66f94ad8f72468e863b9f9
Deryk <derahner@outlook.com>
2018-10-08 20:34:27 -0700

    Problems complete but verify

--- Assignment 5 - Problem 2.py
+++ Assignment 5 - Problem 2.py
@@ -7,9 +7,18 @@ T = 293    #Temperature for everything else
 T_m = 298    #Temperature for mecury
 p = 1
 R_cr = 5*(10**5)
+V = 3.5
 
-"""Interpolation"""
-print("Key","[Air,Water,Isobutane,Mercury]")
+"""Table Values"""
+print("Key","[Air,Water,Mercury]")
+nu_air = 15*(10**-6)            #nu for air from table @300K
+nu_water = (959*(10**-6))/(998) #nu for water from table @265K
+nu_mercury = .1125              #nu for mercury from table @300K
+
+nu = np.array([nu_air,nu_water,nu_mercury])
+
+L = (nu*R_cr)/V
+print(L,"meters")
 
 
 

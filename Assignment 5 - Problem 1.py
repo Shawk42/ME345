f6bc6f63114749689146673f04481ed75095a347
Deryk <derahner@outlook.com>
2018-10-08 20:19:52 -0700

    Problem 1 Correct, Problem 2 not started

--- Assignment 5 - Problem 1.py
+++ Assignment 5 - Problem 1.py
@@ -1,43 +1,23 @@
 """Assignment 5 - Problem 1"""
-import numpy as np
-import matplotlib.pyplot as plt
 
 """Givens"""
 p = 1              #given pressure in [atm]
 T = 25+273.15             #given temperature in [K]
 V = 25             #given velocity in m/s
 Re_cr = 5*(10**5)  #Critical reynolds number
+L = .5
 
-"""Linear Interpolation - From Table in textbook"""
-
-print("Interpolation for Density")
-x_1 = float(input("What is the low temperature"))
-x_2 = T
-x_3 = float(input("What is the high temperature"))
-y_1 = float(input("What is the low density"))
-y_3 = float(input("What is the high density"))
-y_2 = (((x_2-x_1)*(y_3-y_1))/(x_3-x_1))+y_1
-rho = y_2
-
-print("Interpolation for v - At same temperature")
-y_1 = float(input("What is the low v"))
-y_3 = float(input("What is the high v"))
-y_2 = (((x_2-x_1)*(y_3-y_1))/(x_3-x_1))+y_1
-v = y_2
-
-print(rho,"rho")
-print(v,"v")
+"""Table Inputs @300K """
+nu = 15.89*(10**-6)      #nu of air at 300K
 
 """Calculations"""
-L = (v*Re_cr)/V
-print(L,"Length in meters")
+Re = (V*L)/nu         #reynolds number
+p = int((Re/Re_cr)*100)    # % calculation
 
-"""Plotting"""
-L_g = np.linspace(0,.5,num=500)
-Re = (V*L_g)/v
-plt.plot(L_g,Re)
-plt.plot(Re_cr)
-plt.show()
-
-#Found Length not if laminar or not
-print("INCORRECT")
+"""Answer Logic"""
+if Re < Re_cr:
+    print("Flow is Laminar Re is less than Re_cr")
+    print("Re is",p,"% of R_cr")
+else:
+    print("Flow is turbulent as Re is greater than Re_cr")
+    print("Re is", p, "% of R_cr")

2776ce661ebdc85944c8c05c529e72691caf2bb5
Deryk <derahner@outlook.com>
2018-10-25 19:21:09 -0700

    Assignemnt 7 - Problem 1 Complete, Problem 2 Incorrect

---
+++ Assignment 7 - Problem 2.py
@@ -0,0 +1,108 @@
+"""Assigment 7 - Problem 2"""
+print("Assigment 7 - Problem 2")
+import numpy as np
+
+'''GIVENS'''
+Kelvin = 273.15
+m_dot = 0.5     #mass flow flow rate [kg/s]
+T_i = 20+Kelvin #Inlet tempearture [Kelvin]
+T_i_c = 20      #Inlet temperature [C]
+T_s = 150+Kelvin #Surface temperature [Kelvin]
+D = 0.05         #Diamter of pipe [meters]
+L = 25           #Length of pipe [meters]
+
+'''ASSUMPTIONS'''
+T_o_a = (2*T_i)   #Outlet temperature assumption
+T_avg_a = (T_i+T_o_a)/2
+
+'''FLUID PROPERTIES'''
+"""Interpolation X=temp, Y=property"""
+#x_1 = 300
+#x_2 = T_avg_a
+#x_3 = 310
+"""Cp interpolation"""
+#y_1 = 1.909
+#y_3 = 1.951
+#y_2 = (((x_2-x_1)*(y_3-y_1))/(x_3-x_1))+y_1
+#Cp = y_2
+"""Pr interpolation"""
+#y_1 = 6400
+#y_3 = 3400
+#y_2 = (((x_2-x_1)*(y_3-y_1))/(x_3-x_1))+y_1
+#Pr = y_2
+"""Mu interpolation"""
+#y_1 = 48.6*(10**-2)
+#y_3 = 25.3*(10**-2)
+#y_2 = (((x_2-x_1)*(y_3-y_1))/(x_3-x_1))+y_1
+#mu = y_2
+
+"""Values at T = 430K"""
+Cp = 2.471
+Pr = 88
+mu = 0.470*(10**-2)
+K = 132*(10**-3)            #Thermal Conductivity [constant value in range]
+Re = (4*m_dot)/(np.pi*D*mu) #Reynolds number calcations
+x_f = 0.05*Re*D             #Distance to fully developed flow
+x_t = 0.05*Re*Pr*D          #Distance to fully developed thermal BL
+
+'''INTERMEDIATE CALCULATIONS'''
+G_zd = (D/L)*Re*Pr
+a = 0.0668*G_zd
+b = 1+0.04*G_zd**(2/3)
+Nu_d = 3.66+(a/b)
+h = (K*Nu_d)/D
+
+'''OUTLET TEMP CALCULATION'''
+P = np.pi*D               #Perimeter length of pipe
+d = (-(P*L)/(m_dot*Cp))
+c = T_s-T_i
+T_o = (-np.e**d)*c+T_s
+
+'''q calculations'''
+area = np.pi*D*L
+q = h*area*(T_s-T_o)
+
+'''LOGIC'''
+#Flow type
+if Re < 2300:
+    print("Flow is laminar - ",Re)
+else:
+    print("Flow is turbulent - ",Re)
+
+#Boundry layer forming
+if Pr > 1 and x_f < x_t:
+    print("BL formed correctly")
+elif Pr < 1 and x_f > x_t:
+    print("BL formed correctly")
+else:
+    print("BL forming conditions INVALID")
+
+#Pr check for equations
+if Pr >= 5:
+    print("Nu_d equation is valid")
+else:
+    print("Nu_d equation is INVALID")
+
+'''Printing'''
+print("-"*50)
+print("INTERMEDIATE VALUES")
+print("-"*50)
+print("Assumed outlet temp = ",T_o_a)
+print("Assumed mean temp = ", T_avg_a )
+print("Distance to fully developed flow =",x_f)
+print("Distance to thermal BL devloped =", x_t)
+print("Nu_d =",Nu_d)
+print("h =",h)
+print("Outlet Temperature =",T_o)
+print("Heat transfer rate =", q)
+print("Actual minus estimate =", T_o-T_o_a)
+
+'''PROCESS PRINTING'''
+print("-"*50)
+print("PROCESS PRINTING")
+print("-"*50)
+print("The fluid enters the pipe at",T_i)
+print("The pipe is surrounded by a constant temperature of",T_s)
+print("It was assumed that the fluid exits the pipe at",T_o_a)
+print("The fluid was then caclulated to exit the pipe at ",T_o)
+print("The fluid was transfering heat out the pipe into the surface at a rate of",q)
